bd0aee1df62e53a0c963c7e1aae8af1dee73bb7b
BORA EMRE YETER (Kurumsal Cozumler ve Yonetisim Grup Dir. (Calisan Deneyimi Cozumleri Md.) - Danisman) <OTBBYETER@THY.COM>
2025-01-27 17:50:24 +0300

    chore: update package.json exports and refactor type exports in index.tsx; change module resolution to bundler

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,15 @@
 import StackSwiper from './stack';
 
+export type {
+  StackSwiperRef,
+  LayoutEachProps,
+  LayoutProps,
+  SWIPE_POSITION,
+  SWIPE_POSITIONS_INDEX,
+  SWIPE_POSITION_KEY_INDEX,
+  SelectionRefProps,
+  StackEachProps,
+  SwipeCardProps,
+} from './types';
+
 export { StackSwiper };
